badcf747be8a8dd42e51065be68134f074bb4853
Aydan Amorim <aydanamorim@gmail.com>
2024-04-20 17:17:29 -0300

    Incluíndo os primeiros arquivos do projeto no github e configurando o ambiente

---
+++ src/main/java/com/customers/management/system/customersmicroservice/CustomersMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.customers.management.system.customersmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customers/management/system/customersmicroservice/CustomersMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customers.management.system.customersmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
