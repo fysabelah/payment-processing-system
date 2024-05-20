38219391669617b1cf5cd0a8cb12293414f68423
Isabela França <fysabelah@gmail.com>
2024-05-20 15:40:15 -0300

    Adicionar servidor ao swagger

--- src/main/java/com/customers/management/system/customersmicroservice/util/config/SwaggerConfig.java
+++ src/main/java/com/customers/management/system/customersmicroservice/util/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.customers.management.system.customersmicroservice.util.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,6 @@ public class SwaggerConfig {
                                 .title("Customer Management System")
                                 .description("Microservice to management customers - FIAP Fase 04")
                                 .version("1.0.0")
-                );
+                ).addServersItem(new Server().url("http://localhost:7071/order-management-system/customers-microservice"));
     }
 }
