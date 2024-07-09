842d59f65ebd098c5d8db6f7e593e1776b978d08
Isabela França <fysabelah@gmail.com>
2024-07-09 17:01:20 -0300

    Corrigir erro de CORS e adicionar rota dinâmica para os serviços

---
+++ src/main/java/com/order/management/system/gateway/config/GatewayCorsConfig.java
@@ -0,0 +1,26 @@
+package com.order.management.system.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class GatewayCorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
